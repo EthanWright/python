60d9a5ee018fe04d692f194df7f3d871a472541b
Ethan Wright <ethancwright89@gmail.com>
2020-06-10 04:57:59 -0700

    Adding a few more python things

---
+++ facebook_api/credentials.py
@@ -0,0 +1,17 @@
+import configparser
+
+
+class Credentials(object):
+    """
+    Load API Credentials
+    """
+    credentials_file_path = 'input/credentials.txt'
+
+    def __init__(self, api):
+        self.api = api
+
+        self.parser = configparser.ConfigParser()
+        self.parser.read(self.credentials_file_path)
+
+    def get(self, field):
+        return self.parser.get(self.api, field)
