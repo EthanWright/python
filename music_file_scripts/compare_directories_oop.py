3c252dcd6e9ba2dc4e057b46c2c4901fa4aa3aae
Ethan Wright <ethancwright89@gmail.com>
2020-06-15 10:20:27 -0700

    Adding more python scripts and files

---
+++ music_file_scripts/compare_directories_oop.py
@@ -0,0 +1,104 @@
+"""
+Compare files between two directories and move the files if needed
+
+Ethan Wright - 6/11/20
+"""
+
+import copy
+import os
+
+from common import list_music_files, move_file, remove_file_extension
+from fix_names import FixFileNames
+from paths import MUSIC_DIR
+
+
+class MusicFileList(object):
+    ffn = FixFileNames()
+
+    def __init__(self, directory, good_folder, bad_folder):
+        self.directory = directory
+        self.target_good = os.path.join(directory, good_folder)
+        self.target_bad = os.path.join(directory, bad_folder)
+        self.music_list_pointer = 0
+        self.sorted_music_list = self._get_sorted_music_list()
+        self.total_items = len(self.sorted_music_list)
+        self.current_song = None
+        self.current_song_simplified = None
+
+        self._set_current_song()
+
+    def _get_sorted_music_list(self):
+        return sorted(list_music_files(self.directory), key=self.simplify_file_name_fuzzier)
+
+    def move_current_song_match_found(self, result_string=None):
+        result_string = '   MATCHED'
+        self._move_current_song(self.target_good, result_string=result_string, commit=False)
+
+    def move_current_song_no_match(self, result_string=None):
+        result_string = 'X NO MATCH'
+        self._move_current_song(self.target_bad, result_string=result_string, commit=False)
+
+    def _move_current_song(self, target_directory, result_string=None, commit=False):
+        file_name = self.current_song
+        if result_string:
+            print(f'{result_string}: {file_name}')
+        # move_file(file_name, self.directory, target_directory, verbose=result_string is not None, commit=commit)
+        self._increment_position()
+
+    def _set_current_song(self):
+        if self.music_list_pointer < self.total_items:
+            result = self.sorted_music_list[self.music_list_pointer]
+            self.current_song = result
+            self.current_song_simplified = self.simplify_file_name_fuzzier(result)
+        else:
+            self.current_song = None
+            self.current_song_simplified = None
+
+    def _increment_position(self):
+        self.music_list_pointer += 1
+        self._set_current_song()
+
+    def simplify_file_name_fuzzier(self, name):
+        return self.ffn.get_new_name(remove_file_extension(name)).lower()[:22]
+
+
+def sort_directory_contents(directory_1, directory_2):
+    matches_count = 0
+
+    music_file_list_1 = MusicFileList(directory_1, 'replacement_found', 'missing')
+    music_file_list_2 = MusicFileList(directory_2, 'liked_replacements', 'a')
+
+    while music_file_list_1.current_song and music_file_list_2.current_song:
+
+        simple_string_1 = music_file_list_1.current_song_simplified
+        simple_string_2 = music_file_list_2.current_song_simplified
+
+        min_len = min(len(simple_string_1), len(simple_string_2))
+        simple_string_1 = simple_string_1[:min_len]
+        simple_string_2 = simple_string_2[:min_len]
+
+        if simple_string_1 < simple_string_2:
+            music_file_list_1.move_current_song_no_match()
+
+        elif simple_string_1 > simple_string_2:
+            music_file_list_2.move_current_song_no_match()
+
+        elif simple_string_1 == simple_string_2:
+            music_file_list_1.move_current_song_match_found()
+            music_file_list_2.move_current_song_match_found()
+            matches_count += 1
+
+    while music_file_list_1.current_song:
+        music_file_list_1.move_current_song_no_match()
+
+    while music_file_list_2.current_song:
+        music_file_list_2.move_current_song_no_match()
+
+    print(f'Matches: {matches_count}')
+
+
+if __name__ == '__main__':
+    # existing_music = os.path.join(MUSIC_DIR, r'liked')
+    existing_music = os.path.join(MUSIC_DIR, r'to_sort\to_sort_original')
+    new_music = os.path.join(MUSIC_DIR, r'to_sort\to_sort_redo')
+    sort_directory_contents(existing_music, new_music)
---
+++ youtube_song_downloader/song_downloader.py
@@ -0,0 +1,153 @@
+"""
+This contains a Wrapper around the youtube-dl python project
+It reads video ids from a file, and then downloads them from
+youtube using predetermined settings.
+It only saves the audio, not the video.
+It downloads the highest quality that it is able to.
+
+Ethan Wright 6/15/20
+"""
+
+import os
+import re
+import time
+import datetime
+import youtube_dl
+
+from video_ids_db import DB
+
+position_file = r'input\position.txt'
+TEN = 10
+
+
+def retrieve_all_video_ids_from_file(input_file):
+    video_id_list = []
+    with open(input_file, 'r') as f:
+        for video_id in f:
+            if not video_id:
+                continue
+            video_id_list.append(video_id.strip())
+    exit("Confirm")
+    call_youtube_dl(video_id_list)
+
+
+def retrieve_one_video_id_from_file_tracked():
+    position = int(get_position())
+    print('Position: ' + str(position))
+    video_id = get_next_line_from_file_tracked(position)
+    set_position(position + 1)
+    try:
+        call_youtube_dl([video_id])
+    except FileNotFoundError:
+        set_position(position)  # Rollback
+
+
+def get_next_line_from_file_tracked(position):
+    with open(input_file, 'r') as f:
+        count = 0
+        for video_id in f:
+            count += 1
+            if count <= position:
+                continue
+            video_id = clean_video_id(video_id)
+            if not video_id:
+                position += 1
+                set_position(position + 1)
+                continue
+            return video_id
+    return None
+
+
+def get_position():
+    if not os.path.isfile(position_file):
+        set_position(0)
+    with open(position_file, 'rb') as read_file:
+        position = read_file.readlines()[0]
+    return position
+
+
+def set_position(new_position):
+    with open(position_file, 'wb') as write_file:
+        write_file.write(bytes(str(new_position), 'utf-8'))
+
+
+def call_youtube_dl(video_id_list):
+
+    ydl_opts = {
+        # 'verbose': True,  # For debugging
+        # 'format': 'worstaudio/worst',  # For testing
+        'format': 'bestaudio/best',  # Obviously
+        # 'outtmpl': '%(title)s - %(artist)s.%(ext)s',  # Edited youtube-dl project to do this
+        'keepvideo': False,
+        'postprocessors': [
+            {'key': 'FFmpegExtractAudio', 'nopostoverwrites': False},  # --extract-audio
+            {'key': 'FFmpegMetadata'},  # --add-metadata
+        ]
+    }
+
+    if not video_id_list:
+        return
+    if isinstance(video_id_list, str):
+        video_id_list = [video_id_list]
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        db = DB()
+        for video_id in video_id_list:
+
+            video_id = clean_video_id(video_id)
+            if not video_id:
+                continue
+            if not extract_video_id(video_id):
+                print(f'Bad Video ID format {video_id}')
+                continue  # ???
+
+            # Check if it has been downloaded
+            if db.exists(video_id):
+                print(f'ERROR! {video_id} already exists!')
+                continue
+
+            print('Downloading: ' + video_id)
+            print('Time: ' + time.asctime())
+
+            try:
+                ydl.download([video_id])
+            except youtube_dl.utils.DownloadError as e:
+                print('ERROR!')
+                print(e)
+                continue
+
+            sleepy_time(3*60)
+
+
+def extract_video_id(video_id):
+    video_id_regex = r'(https://www.youtube.com/watch\?v=)?([0-9a-zA-Z\-\_]{11})'
+    return re.search(video_id_regex, video_id).group(2)
+
+
+def clean_video_id(video_id):
+    if ' ' in video_id:
+        video_id = video_id.split(' ', 1)[0]
+    return video_id.strip()
+
+
+def sleepy_time(seconds_to_sleep=10):
+    seconds_to_sleep = int(seconds_to_sleep)
+    print(f'Sleeping! [{seconds_to_sleep} seconds]', end='', flush=True)
+    sleep_string = '.' * 10
+    for x in range(int(seconds_to_sleep / TEN)):
+        if x % 6 == 0:
+            print('')
+        print(sleep_string, end='', flush=True)
+        time.sleep(TEN)
+
+    time.sleep(int(seconds_to_sleep % TEN))
+    print('\n')
+
+
+if __name__ == '__main__':
+    # input_file = 'input/video_ids.txt'
+    # retrieve_all_video_ids_from_file(input_file)
+
+    input_file = 'input/video_ids_to_listen_to.txt'
+    retrieve_one_video_id_from_file_tracked()
+
