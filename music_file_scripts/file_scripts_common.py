5e038b92c6f325b6d0c9d62c299165f96fdfbf5a
EthanWright <ethancwright89@gmail.com>
2020-11-19 03:08:30 -0800

    Updates for TraverseDirectory

--- music_file_scripts/file_scripts_common.py
+++ music_file_scripts/file_scripts_common.py
@@ -47,12 +47,23 @@ def rename_file_safe(directory, file_name, new_file_name, verbose=True,  commit=
 class TraverseDirectory(object):
 
     def handle_dir(self, dir_name, directory, full_path):
-        raise NotImplemendedError()
+        return True
 
     def handle_file(self, file_name, directory, full_path):
-        raise NotImplemendedError()
+        return True
 
-    def iterate_files_recursively(self, directory):
+    def iterate_files_recursively(self, directory, lazy=False):
+        if lazy:
+            recursive_function = self.iterate_files_recursively_lazy
+        else:
+            recursive_function = self.iterate_files_recursively_normal
+
+        # Check root dir and kick off recursive search
+        if self.handle_dir('', directory, directory):
+            recursive_function(directory)
+
+    def iterate_files_recursively_normal(self, directory):
+
         for file_name in os.listdir(directory):
             full_path = os.path.join(directory, file_name)
 
@@ -61,7 +72,7 @@ class TraverseDirectory(object):
 
             elif os.path.isdir(full_path):
                 if self.handle_dir(file_name, directory, full_path):
-                    self.iterate_files_recursively(full_path)
+                    self.iterate_files_recursively_normal(full_path)
 
     def iterate_files_recursively_lazy(self, directory):
         found_dirs = []
